81bcfe9b505185945b7a07e4b2f807447df0f286
Daniil <ggdanila424@gmail.com>
2026-02-12 12:09:14 +0200

    Rezolvat

---
+++ main.js
@@ -0,0 +1,11 @@
+//7.1
+console.log("7.1")
+import {adunare, scadere} from "./mathUtils";
+
+let suma = adunare(2,6);
+let scadere = scadere(10,2);
+console.log(suma);
+console.log(scadere);
+
+//7.2
+
